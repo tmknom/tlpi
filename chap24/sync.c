163ae6e636f038373eeaca0828a22208385e3c59
tmknom <tmk.nom@gmail.com>
2021-06-26 09:23:55 +0900

    sync.cの実装

---
+++ chap24/sync.c
@@ -0,0 +1,54 @@
+#include <signal.h>
+#include "tlpi_hdr.h"
+
+#define SYNC_SIG SIGUSR1
+
+static void
+handler(int sig) {
+    return;
+}
+
+int
+main(int argc, char *argv[]) {
+    pid_t childPid;
+    sigset_t blockMask, origMask, emptyMask;
+    struct sigaction sa;
+
+    setbuf(stdout, NULL); // disable stdout buffering
+
+    sigemptyset(&blockMask);
+    sigaddset(&blockMask, SYNC_SIG);
+    if (sigprocmask(SIG_BLOCK, &blockMask, &origMask) == -1)
+        errExit("sigprocmask");
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sa.sa_handler = handler;
+    if (sigaction(SYNC_SIG, &sa, NULL) == -1)
+        errExit("sigaction");
+
+    switch (childPid = fork()) {
+        case -1:
+            errExit("fork");
+        case 0: // child
+            printf("[%s %ld] Child started - doiing some work\n", currTime("%T"), (long) getpid());
+            sleep(2);
+            printf("[%s %ld] Child about to signal parent\n", currTime("%T"), (long) getpid());
+
+            if (kill(getppid(), SYNC_SIG) == -1)
+                errExit("kill");
+            _exit(EXIT_SUCCESS);
+        default: // parent
+            printf("[%s %ld] Parent about to wait for signal\n", currTime("%T"), (long) getpid());
+
+            sigemptyset(&emptyMask);
+            if (sigsuspend(&emptyMask) == -1 && errno != EINTR)
+                errExit("sigsuspend");
+            printf("[%s %ld] Parent got signal\n", currTime("%T"), (long) getpid());
+
+            if (sigprocmask(SIG_SETMASK, &origMask, NULL) == -1)
+                errExit("sigprocmask");
+
+            exit(EXIT_SUCCESS);
+    }
+}
