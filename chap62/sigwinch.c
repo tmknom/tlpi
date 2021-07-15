8eb45fd8ce07882dcf5a3f40979d25e15621598c
tmknom <tmk.nom@gmail.com>
2021-07-15 10:53:21 +0900

    sigwinch.cの実装

---
+++ chap62/sigwinch.c
@@ -0,0 +1,27 @@
+#include <signal.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include "tlpi_hdr.h"
+
+static void
+handler(int sig) {}
+
+int
+main(int argc, char *argv[]) {
+    struct winsize ws;
+    struct sigaction sa;
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = handler;
+    if (sigaction(SIGWINCH, &sa, NULL) == -1)
+        errExit("sigaction");
+
+    for (;;) {
+        pause();
+
+        if (ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) == -1)
+            errExit("ioctl");
+        printf("Caught SIGWINCH: %d rows * %d columns\n", ws.ws_row, ws.ws_col);
+    }
+}
